ab2f2d0d72a49008cb9f77f504553947b2ee3386
kirirotha <kirirotha@gmail.com>
2020-09-21 11:45:10 -0500

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,19 @@
-function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
-}
+// function insertionSort(arr) {
+//   // your code here!
+//   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  
+// }
+
+let insertionSort = (inputArr) => {
+  let length = inputArr.length;
+  for (let i = 1; i < length; i++) {
+      let key = inputArr[i];
+      let j = i - 1;
+      while (j >= 0 && inputArr[j] > key) {
+          inputArr[j + 1] = inputArr[j];
+          j = j - 1;
+      }
+      inputArr[j + 1] = key;
+  }
+  return inputArr;
+};
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,30 @@
-function selectionSort(arr) {
+//function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  // arr.sort((a, b) => {
+  //   return a - b
+  // });
+  // console.log(arr)
+  // debugger
+// }
+
+function selectionSort(arr) {
+
+  for (var i = 0; i < arr.length; i++) {
+
+      let min = i; //  storing the index of minimum element
+
+      for (var j = i + 1; j < arr.length; j++) {
+          if (arr[min] > arr[ j ]) {
+              min = j; // updating the index of minimum element
+          }
+      }
+
+      if (i !== min) {
+          let temp = arr[ i ];
+          arr[ i ] = arr[min];
+          arr[min] = temp;
+      }
+  }
+  return arr;
 }
